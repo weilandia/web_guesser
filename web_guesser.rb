90217c46c0155c13ee18a331d09b5947795ddf56
Nick Weiland <nickweiland@gmail.com>
2016-01-26 00:42:02 -0700

    Add button change

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,6 +4,8 @@ require 'sinatra/reloader'
 N = rand(1..100)
 
 def message
+  @button_class = 'btn btn-primary'
+  @button_value = 'Guess!'
   if params["guess"].to_i == 0 then "Guess a number between 1 and 100."
   elsif params["guess"].to_i > N + 5
     @form_ouput = "background-color: #F22613"
@@ -20,6 +22,8 @@ def message
   else
     @form_ouput = "display: none"
     @win_background = "background-color: #3FC380"
+    @button_class = 'btn btn-success'
+    @button_value = 'Play again?'
     "You got it right!"
   end
 end
@@ -29,5 +33,5 @@ def secret
 end
 
 get '/' do
-  erb :index, :locals => { :secret => secret, :message =>   message, :form => @form_ouput}
+  erb :index, :locals => { :secret => secret, :message =>   message, :form => @form_ouput, :button_class => @button_class, :button_value => @button_value}
 end
