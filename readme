6ccafe668fb2f07073f9b1be96f196b71d3df866
Nick Weiland <nickweiland@gmail.com>
2016-01-26 00:18:22 -0700

    Add colored outputs

---
+++ readme
@@ -0,0 +1,3 @@
+# web-guesser
+
+A simple Sinatra application for Turing School Module 2.
--- web_guesser.rb
+++ web_guesser.rb
@@ -1,13 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-N = rand(100)
+N = rand(1..100)
 
 def message
-  params["guess"].to_i > N + 5 ? "Way too high!" :
-  params["guess"].to_i < N - 5 ? "Way too low!" :
-  params["guess"].to_i > N ? "Too high!" :
-  params["guess"].to_i < N ? "Too low!" : "You got it right!"
+  if params["guess"].to_i == 0 then "Guess a number between 1 and 100."
+  elsif params["guess"].to_i > N + 5
+    @form_ouput = "background-color: #F22613"
+    "Way too high!"
+  elsif params["guess"].to_i < N - 5
+    @form_ouput = "background-color: #F22613"
+    "Way too low!"
+  elsif params["guess"].to_i > N
+    @form_ouput = "background-color: #F89406"
+    "Too high!"
+  elsif params["guess"].to_i < N
+    @form_ouput = "background-color: #F89406"
+    "Too low!"
+  else
+    @form_ouput = "display: none"
+    @win_background = "background-color: #3FC380"
+    "You got it right!"
+  end
 end
 
 def secret
@@ -15,5 +29,5 @@ def secret
 end
 
 get '/' do
-  erb :index, :locals => { :secret => secret, :message =>   message }
+  erb :index, :locals => { :secret => secret, :message =>   message, :form => @form_ouput}
 end
